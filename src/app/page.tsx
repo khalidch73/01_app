83028bac48a74a54d8ff3f55b649a4c331da8fdf
khalidch73 <khalidnawazch73@gmail.com>
2023-10-08 21:32:54 +0500

    name up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className='font-bold text-6xl text-blue-500 '>
-        Hello world i am khalid nawaz.
+        Hello world i am khalid nawaz choudhary.
       </h1>
       <p>
         Im from batch 47 and learning web-development from PIAIC.
