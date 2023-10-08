9ca10d0fb4003a350698f09796a60cf8464c53a9
khalidch73 <khalidnawazch73@gmail.com>
2023-10-08 22:32:36 +0500

    up more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Image  from "next/image";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='font-bold text-6xl text-blue-500 '>
-        Hello world i am khalid nawaz choudhary.
+    <main className="flex min-h-screen flex-col items-center p-24 gap-5 bg-indigo-400">
+      <h1 className='font-bold text-6xl text-blue-500'>
+        Hello world i am khalid nawaz bandesha.
       </h1>
       <p>
         Im from batch 47 and learning web-development from PIAIC.
